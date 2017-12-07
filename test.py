c44cb225867e3a442884ef6aeb846c3a38907a80
Maxime Mazouth--Laurol <maxime.mazouth--laurol@ensimag.grenoble-inp.fr>
2017-12-07 20:08:53 +0100

    planning updated + radonTransform function modified to use the formula linking radonTransform(rT) of a scaled disk with rT of unit disk

--- test.py
+++ test.py
@@ -37,41 +37,30 @@ from project import *
 #print("the number of image in this fram is " , fram1.nb_of_image()) 
 #
 #
-# #test isInCircle
-##expected result : True, False, False, False, False
-center1 = np.array([2,2]); radius1 = 2; intensity1 = 0;
-C1 = Circle(center1, radius1, intensity1)
-point1 = np.array([2,4]); point2 = np.array([2,4.1]); point3 = np.array([2,-0.1]); point4 = np.array([-0.1,2]); point5 = np.array([4,1.5])
-list_points = [point1,point2,point3,point4,point5]
-for point in list_points:
-    print(C1.isInCircle(point))
 
-#test lengthLineIntersection
-#expected result : 4.0
-line2 = np.array([(2,0.25*i) for i in range (0,10)])
-print(C1.lengthLineIntersection(line2))
+
 
 frame = Framework(0,0,5)
-line1 = line(frame, np.pi/2 , 2 ,0.1)
-print(C1.lengthLineIntersection(line1))
+a = 100 #nb of subdivisions of phi
+m = 1000 #nb of subdivisions of s
 
 #test Sinogram
-center2 = np.array([0,0]); radius2 = 2; intensity2 = 2;
+center2 = np.array([0,0]); radius2 = 1; intensity2 = 2;
 C2 = Circle(center2, radius2, intensity2)
-line2 = line(frame,0,-10,0.1)
-print(C2.lengthLineIntersection(line2))
+
 img1 = Image()
 img1.addCircle(C2)     
-#center2 = np.array([1,1]); radius2 = 1; intensity2 = 4;
-#C2 = Circle(center2, radius2, intensity2)
+center2 = np.array([1,1]); radius2 = 2; intensity2 = 4;
+C2 = Circle(center2, radius2, intensity2)
 #img1.addCircle(C2)
 frame.addImage(img1)
 frame.setCurrentImage(0)
 
-radonTransform = np.zeros((100,100))
-radonTransform = buildSinogram(frame,100,100,0.1)
+sinoMatrix= np.zeros((a,m))
+sinoMatrix = sinogram(frame,a,m)
+plotSinogram(frame,sinoMatrix)
 #test constantness of radonTransform for a circle
-test = np.copy(radonTransform)
+test = np.copy(sinoMatrix)
 for k in range(1,np.shape(test)[0]):
     test[k] = test[k] - test[0]
 test[0] = np.zeros(np.shape(test[0]))
@@ -84,23 +73,25 @@ res = integrale(x,y)
 print(res)
 
 #test moment function
-order = 4
+order = 5
 plt.figure()
-vecMoment = moment(frame,radonTransform,order)
+vecMoment = moment(frame,sinoMatrix,order)
 absPhi = np.linspace(0,2*np.pi,len(vecMoment))
 plt.plot(absPhi,vecMoment)
 
 #testDotProduct
-phi = np.linspace(0,2*np.pi,100)
+phi = np.linspace(0,2*np.pi,a)
 vecP = np.sin(5*phi)
 vecQ = np.sin(5*phi)
-res = trigoDotProduct(phi,np.ones(100),np.ones(100))
+res = trigoDotProduct(phi,np.ones(a),np.ones(a))
 print(res)
 #we derive consistent and precise values for norms of basis trigonometric 
 #polynomial functions
 
 #test projectionMoment
 vecProj = projectionMoment(phi,order,vecMoment)
+plt.figure()
+plt.plot(phi,vecProj)
 print(np.linalg.norm(vecMoment-vecProj))
 
 
